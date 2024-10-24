7076904c63e1f09a1ea96fc0b0a55115e5190e6c
Toan <hongtoan20042000@gmail.com>
2024-10-24 15:48:29 +0700

    test image

--- src/component/SliderComponents.jsx
+++ src/component/SliderComponents.jsx
@@ -2,11 +2,10 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import TextContent from "./TextContent";
-import * as images from "../assets/images"; // Import all images as an object
 const SliderComponent = () => {
   return (
     <div className="slider-container">
-      <img src={images.image18} className="w-full  object-cover no-caret" />
+      <img src="https://res.cloudinary.com/dpohykmqq/image/upload/v1729759646/1DC02498_fft5ga.jpg" className="w-full  object-cover no-caret" />
       <TextContent />
     </div>
   );
